0c03f37e17530389c8dabdee463ced1a2b1ebe13
patricedjate <patricedjate8@gmail.com>
2024-10-26 10:07:58 +0000

    definitive commit

--- app/src/main/java/com/example/limonade/MainActivity.kt
+++ app/src/main/java/com/example/limonade/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -30,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -70,6 +74,16 @@ fun LemonWithButtonAndImage(modifier : Modifier = Modifier){
         3 -> R.string.re3
         else -> R.string.re4
     }
+    Column (
+        modifier
+            .background(color = Color.Yellow)
+            .fillMaxWidth(),
+   horizontalAlignment = Alignment.CenterHorizontally,
+    ){
+        Text("Limonade")
+
+
+    }
   Column (
       modifier
       .fillMaxSize()
@@ -89,7 +103,7 @@ fun LemonWithButtonAndImage(modifier : Modifier = Modifier){
                        // result+=1
 
                         if(result==1){
-                            tap = (1..6).random()
+                            tap = (2..6).random()
                             result+=1
                         }
                         if(result==2 && tap==0){
